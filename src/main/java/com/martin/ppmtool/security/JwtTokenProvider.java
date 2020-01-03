aae5d863d075f0f02ef8e54c911a2dc38365420a
Martin Miralles <martinmiralles66@gmail.com>
2020-01-03 18:39:27 -0500

    Created Jwt filters when using tokens

--- src/main/java/com/martin/ppmtool/security/JwtTokenProvider.java
+++ src/main/java/com/martin/ppmtool/security/JwtTokenProvider.java
@@ -1,8 +1,7 @@
 package com.martin.ppmtool.security;
 
 import com.martin.ppmtool.domain.User;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
@@ -41,7 +40,30 @@ public class JwtTokenProvider {
 
 
     //Validate the token
+     public boolean validateToken(String token){
+        try{
+            Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token);
+            return true;
+        } catch (SignatureException ex){
+            System.out.println("Invalid JWT Signature");
+        } catch (MalformedJwtException ex){
+            System.out.println("Invalid JWT Token");
+        } catch (ExpiredJwtException ex){
+            System.out.println("Expired JWT Token");
+        } catch(UnsupportedJwtException ex){
+            System.out.println("Unsupported JWT Token");
+        } catch (IllegalArgumentException ex){
+            System.out.println("JWT claims string is empty");
+        }
+        return false;
+     }
 
     //Get User ID from token
+    public Long getUserIdFromJWT(String token){
+        Claims claims = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody();
+        String id = (String)claims.get("id");
+
+        return Long.parseLong(id);
+    }
 
 }
